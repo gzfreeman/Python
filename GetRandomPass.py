c8fbaef653f8065f7ffe58f5a20f1dcde59569e3
gzfreeman <marco.freeman@gmail.com>
2018-08-31 00:19:39 +0800

    优化0831

--- GetRandomPass.py
+++
@@ -1,9 +0,0 @@
-##随机生成密码
-from random import choice
-import string
-
-def GenPassword(length=8,chars=string.ascii_letters+string.digits):
-    return ''.join([choice(chars) for i in range(length)])
-
-for i in range(1):
-    print(GenPassword(8))
